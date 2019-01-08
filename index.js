03646fea2ce4769fa3887f4a3446674d830f1e9d
Jeevan Prakash <jiwanprakash65@gmail.com>
2019-01-08 22:15:18 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const brain = require("brain.js");
+
+const net = new brain.NeuralNetwork();
+
+net.train([
+  { input: [0, 0, 0], output: [0] },
+  { input: [0, 0, 1], output: [0] },
+  { input: [0, 1, 1], output: [0] },
+  { input: [1, 0, 1], output: [1] },
+  { input: [1, 1, 1], output: [1] }
+]);
+
+const output = net.run([1, 0, 1]);
+
+console.log(output);
